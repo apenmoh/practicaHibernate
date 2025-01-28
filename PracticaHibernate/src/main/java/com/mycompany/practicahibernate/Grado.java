d368800f90ddd47eb710a1499c79f9df211b74c1
apenmoh <167619359+apenmoh@users.noreply.github.com>
2025-01-28 10:21:38 +0100

    .

--- PracticaHibernate/src/main/java/com/mycompany/practicahibernate/Grado.java
+++ PracticaHibernate/src/main/java/com/mycompany/practicahibernate/Grado.java
@@ -2,7 +2,15 @@ package com.mycompany.practicahibernate;
 
 import java.util.List;
 
-import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "grado")
