a9d2105445a73f584ebafb65cfb6b7c14850b08c
apenmoh <167619359+apenmoh@users.noreply.github.com>
2025-01-30 13:17:54 +0100

    Estudiante Corrigido

--- PracticaHibernate/src/main/java/com/mycompany/practicahibernate/Estudiante.java
+++ PracticaHibernate/src/main/java/com/mycompany/practicahibernate/Estudiante.java
@@ -1,8 +1,14 @@
-package com.mycompany;
+package com.mycompany.practicahibernate;
 
-import jakarta.persistence.*;
 import java.util.Set;
-import com.mycompany.practicahibernate.curso_escolar;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "estudiante")
